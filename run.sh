8bce7358a57df53d9dfe1e31b719e0246054a5e1
cyang019 <yangcnju@gmail.com>
2017-10-30 00:24:17 -0400

    add test case 2

--- run.sh
+++ run.sh
@@ -1,27 +1,30 @@
 #!/bin/bash
 
 # ---------------------------------------------------------------------
-# python implementation
-# Uncomment the line below and comment out everything else for executing the python version.
-python ./src/find_political_donors.py ./raw_input/itcont.txt ./output/python_zip.txt ./output/python_date.txt
+## python implementation
+## Uncomment the line below and comment out everything else for executing the python version.
+#python ./src/find_political_donors.py ./input/itcont.txt ./output/medianvals_by_zip.txt ./output/medianvals_by_date.txt
 # ---------------------------------------------------------------------
 
 
 # ---------------------------------------------------------------------
-# cpp implementation
+## cpp implementation
 ## Uncomment the lines below with a single '#' to use cpp implementations
 ####################
-## use cmake to compile
-#mkdir build
-#cd build
-#cmake ../src/donation_analysis_cpp
-#
-## make will generate executable in the parent directory
-#make
+## use cmake to compile if file not already compiled
+if [ ! -e find_political_donors ]
+then
+    mkdir build
+    cd build
+    cmake ../src/donation_analysis_cpp
+
+    ## make will generate executable in the parent directory
+    make
+fi
 #####################
 #
 ## change back to parent directory
-#cd ..
+cd ..
 ## execute
-./find_political_donors ./raw_input/itcont.txt ./output/cpp_zip.txt ./output/cpp_date.txt
+./find_political_donors ./input/itcont.txt ./output/medianvals_by_zip.txt ./output/medianvals_by_date.txt
 # ---------------------------------------------------------------------
